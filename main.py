fc4be58d46cb283965aa096d37d1677bb0e80e36
yokotanaohiko <iam4570@example.com>
2017-08-12 19:34:45 +0900

    fix

--- main.py
+++ main.py
@@ -8,18 +8,17 @@ from flask import Flask, request, jsonify
 
 app = Flask(__name__)
 
-APP_CONSUMER_SECRET = os.environ.get('CONSUMER_SECRET', 'dummy secret')
+APP_CONSUMER_SECRET = os.environ.get('CONSUMER_SECRET', 'dummy secret').encode()
 
 @app.route('/', methods=['GET'])
 def crc():
-    try:
-        crc_token = request.args['crc_token']
-        sha256_hash_digest = hmac.new(APP_CONSUMER_SECRET, msg=crc_token, digestmod=hashlib.sha256).digest()
-        return jsonify(
-            response_token='sha256=' + base64.b64encode(sha256_hash_digest)
-        )
-    except Exception as e:
-        return 'error' + str(e)
+    crc_token = request.args['crc_token'].encode()
+    print(crc_token)
+    sha256_hash_digest = hmac.new(APP_CONSUMER_SECRET, msg=crc_token, digestmod=hashlib.sha256).hexdigest()
+    print(sha256_hash_digest)
+    return jsonify(
+        response_token=(b'sha256=' + base64.b64encode(sha256_hash_digest.encode())).decode()
+    )
 
 if __name__ == '__main__':
     app.run()
