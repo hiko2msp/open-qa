068123e17acf27a8b33882dd04e1830c074c4148
hiko2msp <hiko2msp@gmail.com>
2017-08-12 18:56:48 +0900

    append

--- main.py
+++ main.py
@@ -1,10 +1,18 @@
+import os
+
+import base64
+import hmac
+import hashlib
+
 from flask import Flask
 
 app = Flask(__name__)
 
+# sha256_hash_digest = hmac.new()
+
 @app.route('/')
 def hello():
-    return 'Hello World!'
+    return 'Hello World!' + str(os.environ.get('TEST', 'no test'))
 
 if __name__ == '__main__':
     app.run()
